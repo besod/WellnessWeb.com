7efc65a27e2403f632f819382269ef4d0ed74c1f
Benjamin <bini2nd@yahoo.com>
2023-09-06 22:35:26 +0200

    mixin views added

--- README.md
+++ README.md
@@ -2,4 +2,19 @@
 
 Health and Wellness online Learning Platform
 
-  ###  - the platform offer courses on various subjects. Each course is devided into configurable number of modules, and each module contains a configurable number of contents. The contents have various types: text, files, images, or videos. Therefore the data  model allows to store these diverse content. For this pupose Polymorphism is used, which is the provision of a single interface to entities of different types.
+the platform offer courses on various subjects. Each course is devided into configurable number of modules, and each module contains a configurable number of contents. The contents have various types: text, files, images, or videos. Therefore the data model allows to store these diverse content. For this pupose Polymorphism is used, which is the provision of a single interface to entities of different types.
+
+Functionalities:
+List the courses created by the instructor
+Create, edit, and delete courses
+Add modules to ta course and reorder them
+Add different type of content to each module
+Reorder course modules and content
+
+Lessons:
+Create models for the platform
+Create fixtures for the models and appy them
+Use model inheritance to create data models for polymorphic content
+Create custom model fields
+Order course contents and modules
+Build authentication views
--- edu/courses/views.py
+++ edu/courses/views.py
@@ -1,3 +1,46 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Course
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+
+class OwnerMixin:
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner = self.request.user)
+
+# Implments the form_valid() method, which is used by views that use Django's ModelFormixin mixin    
+class OwnerEditMixin:
+    def form_valid(self, form):
+        form.instance.owner = self.request.user #current user
+        return super().form_valid(form)
+    
+class OwnerCourseMixin(OwnerMixin):
+    model = Course #used for QuerySets. its used by all views
+    fields = ['subject', 'title','slug','overview'] # the fields fo the model to build the model form of the CreateView and UpdateView views 
+    success_url = reverse_lazy('manage_course_list') #redirects the user after the form is successfully submitted or object is deleted
+
+#The templage_name attribute is used for CreateView and UpdateView views
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
+    template_name= 'courses/manage/course/form.html'
+
+#inherits from OwnerCourseMixin and ListView. The template listes course
+class ManageCourseListView(OwnerCourseMixin, ListView):
+    template_name = 'courses/manage/course/list.html'
+
+#inherits from OwnerCourseMixin and and Createview. It uses the template defined in OwnerCourseMixin 
+class CourseCreateView(OwnerCourseEditMixin, CreateView):
+    pass 
+
+#inherits from OwnerCourseMixin and and Updateview. It uses the template defined in OwnerCourseMixin 
+class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
+    pass 
+
+#inherits from OwnerCourseMixin and and Deleteview. It uses the template to confirm the course delition 
+class CourseDeleteView(OwnerCourseEditMixin, DeleteView):
+    template_name = 'courses/manage/course/delete.html'
+
