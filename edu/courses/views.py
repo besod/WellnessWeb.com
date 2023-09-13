adbfb44703ea7a1caca2adca0a6c9f4ec760b508
Benjamin <bini2nd@yahoo.com>
2023-09-13 23:04:47 +0200

    course app fixed

--- edu/courses/views.py
+++ edu/courses/views.py
@@ -13,6 +13,9 @@ from django.forms.models import modelform_factory
 from django.apps import apps
 from .models import Module, Content
 from braces.views import CsrfExemptMixin,JsonRequestResponseMixin
+from django.db.models import Count
+from .models import Subject
+from django.views.generic.detail import DetailView
 
 class ManageCourseListView(ListView):
     model = Course
@@ -61,7 +64,22 @@ class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
 class CourseDeleteView(OwnerCourseMixin, DeleteView):
     template_name = 'courses/manage/course/delete.html'
     permission_required = 'courses.delete_course' 
-
+#inerits from Templateresponsemixin 
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name='courses/course/list.html'
+    def get(self, request, subject=None):
+        
+        subjects = Subject.objects.annotate(total_courses=Count('courses'))
+        courses=Course.objects.annotate(total_modules=Count('modules'))
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses=courses.filter(subject=subject)
+        return self.render_to_response({'subjects':subjects, 'subject':subject,'courses':courses})
+    
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
     
 '''This module handles the formset to add, update, and delete modules for courses
 It inherits from TemplateResponseMixin, which renders templates and returns HTTP response.
